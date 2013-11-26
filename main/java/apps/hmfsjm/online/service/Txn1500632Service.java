e988a75405b19a339616008ca3286f06246fd9ea
张晓波 <izhangxiaobo@gmail.com>
2013-11-26 15:43:58 +0800

    票据查询

--- main/java/apps/hmfsjm/online/service/Txn1500632Service.java
+++ main/java/apps/hmfsjm/online/service/Txn1500632Service.java
@@ -5,6 +5,7 @@ import apps.hmfsjm.repository.MybatisManager;
 import apps.hmfsjm.repository.dao.common.CommonMapper;
 import apps.hmfsjm.repository.model.VoucherBill;
 import common.utils.StringPad;
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +24,14 @@ public class Txn1500632Service {
 
         SqlSession session = manager.getSessionFactory().openSession();
         CommonMapper mapper = session.getMapper(CommonMapper.class);
+        String useCnt = mapper.qryVchCnt(date8, VoucherStatus.USED.getCode());
+        String delCnt = mapper.qryVchCnt(date8, VoucherStatus.CANCEL.getCode());
         List<VoucherBill> vchList = mapper.qryVoucherBills(date8);
         String vchs = transVchsToStr(vchList);
-        return vchs;
+        if (StringUtils.isEmpty(vchs)) {
+            return vchs;
+        } else
+            return useCnt + "|" + delCnt + "|" + vchs;
     }
 
     private String transVchsToStr(List<VoucherBill> vchList) {
@@ -33,17 +39,26 @@ public class Txn1500632Service {
             return null;
         } else {
             StringBuilder vchBuilder = new StringBuilder();
-            vchBuilder.append(StringPad.rightPad4ChineseToByteLength("票据号", 22, " "));
-            vchBuilder.append("  状态  ");
-            vchBuilder.append(StringPad.rightPad4ChineseToByteLength("缴款单号 ", 22, " "));
-            vchBuilder.append("  金额|");
+            vchBuilder.append(vchList.size());
+            logger.info("查询到票据笔数：" + vchList.size());
+            vchBuilder.append("|");
             for (VoucherBill vb : vchList) {
-                vchBuilder.append(StringPad.rightPad4ChineseToByteLength(vb.getVchnum(), 24, " "));
+                vchBuilder.append(StringPad.rightPad4ChineseToByteLength(vb.getVchnum(), 24, ""));
                 vchBuilder.append(VoucherStatus.valueOfAlias(vb.getVchsts()).getTitle());
                 vchBuilder.append("  ");
-                vchBuilder.append(StringPad.rightPad4ChineseToByteLength(vb.getBillno(), 24, " "));
-                vchBuilder.append(vb.getTxnamt());
-                vchBuilder.append("|");
+                if (StringUtils.isEmpty(vb.getBillno())) {
+                    vchBuilder.append(StringPad.rightPad4ChineseToByteLength("", 24, " "));
+
+                } else {
+                    vchBuilder.append(StringPad.rightPad4ChineseToByteLength(vb.getBillno(), 24, " "));
+                }
+                if (StringUtils.isEmpty(vb.getTxnamt())) {
+                    vchBuilder.append(StringPad.rightPad4ChineseToByteLength("", 24, " "));
+
+                } else {
+                    vchBuilder.append(StringPad.rightPad4ChineseToByteLength(vb.getTxnamt(), 24, " "));
+                }
+                vchBuilder.append(",");
             }
             return vchBuilder.toString();
         }
--- main/java/apps/hmfsjm/repository/dao/common/CommonMapper.java
+++ main/java/apps/hmfsjm/repository/dao/common/CommonMapper.java
@@ -16,7 +16,12 @@ public interface CommonMapper {
             " v.txn_date as txndate, b.txn_amt as txnamt from hmfs_jm_voucher v " +
             " left join hmfs_jm_bill b " +
             " on v.billno = b.billno " +
-            " where v.txn_date = #{date8} " +
-            " order by v.vch_sts, v.txn_date desc")
+            " where v.txn_date = #{date8} and v.vch_sts != '1' " +
+            " order by v.vch_sts, v.vch_num, v.txn_date desc")
     List<VoucherBill> qryVoucherBills(@Param("date8") String date8);
+
+
+    @Select("select  count(v.vch_num) from hmfs_jm_voucher v " +
+            " where v.txn_date = #{date8} and v.vch_sts = #{vchsts} ")
+    String qryVchCnt(@Param("date8") String date8, @Param("vchsts") String vchsts);
 }
